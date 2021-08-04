0ac323fae1abb454b1415349be149c05c2bc77e8
MASHOD0 <mashhood2002@gmail.com>
2021-08-05 01:15:31 +0530

    map init

--- Map1.lua
+++ Map1.lua
@@ -43,3 +43,140 @@ JUMP_BLOCK_HIT = 9
 
 -- a speed to multiply delta time to scroll map; smooth value
 local SCROLL_SPEED = 62
+-- constructor for our map object
+function Map:init()
+
+    self.spritesheet = love.graphics.newImage('graphics/spritesheet.png')
+    self.sprites = generateQuads(self.spritesheet, 16, 16)
+    self.music = love.audio.newSource('sounds/music.wav', 'static')
+
+    self.tileWidth = 16
+    self.tileHeight = 16
+    self.mapWidth = 30
+    self.mapHeight = 28
+    self.tiles = {}
+
+    -- applies positive Y influence on anything affected
+    self.gravity = 15
+
+    -- associate player with map
+    self.player = Player(self)
+
+    -- camera offsets
+    self.camX = 0
+    self.camY = -3
+
+    -- cache width and height of map in pixels
+    self.mapWidthPixels = self.mapWidth * self.tileWidth
+    self.mapHeightPixels = self.mapHeight * self.tileHeight
+
+    -- first, fill map with empty tiles
+    for y = 1, self.mapHeight do
+        for x = 1, self.mapWidth do
+            
+            -- support for multiple sheets per tile; storing tiles as tables 
+            self:setTile(x, y, TILE_EMPTY)
+        end
+    end
+    
+
+    --Declaration of new width
+    NEW_WIDTH = self.mapWidth - WIDTH
+    -- begin generating the terrain using vertical scan lines
+    local x = 1
+    --if x == self.mapWidth  then
+        --draw the pole
+        
+
+
+    while x < self.mapWidth do
+        
+        -- edit reqired
+        -- 2% chance to generate a cloud
+        -- make sure we're 2 tiles from edge at least
+
+        if x < NEW_WIDTH - 2 then
+            if math.random(20) == 1 then
+                
+                -- choose a random vertical spot above where blocks/pipes generate
+                local cloudStart = math.random(self.mapHeight / 2 - 6)
+
+                self:setTile(x, cloudStart, CLOUD_LEFT)
+                self:setTile(x + 1, cloudStart, CLOUD_RIGHT)
+            end
+        end
+
+    if x == NEW_WIDTH then
+        for i = 0, PYRAMID, 1 do
+            START = NEW_WIDTH + i
+            for j= 0,PYRAMID - i , 1 do
+                self:setTile(START + j, self.mapHeight/2 - i -1 , TILE_BRICK)
+            end
+        end
+    
+        
+            self:setTile(self.mapWidth -2, self.mapHeight/ 2 -1 , POST_3)
+            self:setTile(self.mapWidth -2, self.mapHeight/ 2 -2, POST_2)
+            self:setTile(self.mapWidth -2, self.mapHeight/ 2 -3 , POST_1)
+            self:setTile(self.mapWidth -1, self.mapHeight/ 2 -3, FLAG_1)
+            for y = self.mapHeight / 2, self.mapHeight do
+                self:setTile(self.mapWidth, y, TILE_BRICK)
+            end
+
+    
+        
+    end 
+
+        -- edit reqired
+        -- 5% chance to generate a mushroom
+        if (math.random(20) == 1 and x < NEW_WIDTH) then
+            -- left side of pipe
+            self:setTile(x, self.mapHeight / 2 - 2, MUSHROOM_TOP)
+            self:setTile(x, self.mapHeight / 2 - 1, MUSHROOM_BOTTOM)
+
+            -- creates column of tiles going to bottom of map
+            for y = self.mapHeight / 2, self.mapHeight do
+                self:setTile(x, y, TILE_BRICK)
+            end
+
+            -- next vertical scan line
+            x = x + 1
+
+        -- 10% chance to generate bush, being sure to generate away from edge
+        elseif math.random(10) == 1  then
+            local bushLevel = self.mapHeight / 2 - 1
+
+            -- place bush component and then column of bricks
+            self:setTile(x, bushLevel, BUSH_LEFT)
+            for y = self.mapHeight / 2, self.mapHeight do
+                self:setTile(x, y, TILE_BRICK)
+            end
+            x = x + 1
+
+            self:setTile(x, bushLevel, BUSH_RIGHT)
+            for y = self.mapHeight / 2, self.mapHeight do
+                self:setTile(x, y, TILE_BRICK)
+            end
+            x = x + 1
+
+        -- 10% chance to not generate anything, creating a gap
+        elseif (math.random(10) ~= 1 )  then
+            
+            -- creates column of tiles going to bottom of map
+            for y = self.mapHeight / 2, self.mapHeight do
+                self:setTile(x, y, TILE_BRICK)
+            end
+
+            -- chance to create a block for Mario to hit
+            if (math.random(15) == 1 and x < NEW_WIDTH) then
+                self:setTile(x, self.mapHeight / 2 - 4, JUMP_BLOCK)
+            end
+
+            -- next vertical scan line
+            x = x + 1
+        end
+    end
+--t the background music
+    self.music:setLooping(true)
+    self.music:play()
+end
