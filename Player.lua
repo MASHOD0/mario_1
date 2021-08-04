de3d6aaf8f92c904c1ebcf2429d03a24d3c7c1e3
MASHOD0 <mashhood2002@gmail.com>
2021-08-05 01:05:15 +0530

    init function

--- Player.lua
+++ Player.lua
@@ -6,3 +6,171 @@ Player = Class{}
 
 local WALKING_SPEED = 140
 local JUMP_VELOCITY = 400
+
+function Player:init(map)
+    
+    self.x = 0
+    self.y = 0
+    self.width = 16
+    self.height = 20
+
+    -- offset from top left to center to support sprite flipping
+    self.xOffset = 8
+    self.yOffset = 10
+
+    -- reference to map for checking tiles
+    self.map = map
+    self.texture = love.graphics.newImage('graphics/blue_alien.png')
+
+    -- sound effects
+    self.sounds = {
+        ['jump'] = love.audio.newSource('sounds/jump.wav', 'static'),
+        ['hit'] = love.audio.newSource('sounds/hit.wav', 'static'),
+        ['coin'] = love.audio.newSource('sounds/coin.wav', 'static')
+    }
+
+    -- animation frames
+    self.frames = {}
+
+    -- current animation frame
+    self.currentFrame = nil
+
+    -- used to determine behavior and animations
+    self.state = 'idle'
+
+    -- determines sprite flipping
+    self.direction = 'left'
+
+    -- x and y velocity
+    self.dx = 0
+    self.dy = 0
+
+    -- position on top of map tiles
+    self.y = map.tileHeight * ((map.mapHeight - 2) / 2) - self.height
+    self.x = map.tileWidth * 10
+
+    -- initialize all player animations
+    self.animations = {
+        ['idle'] = Animation({
+            texture = self.texture,
+            frames = {
+                love.graphics.newQuad(0, 0, 16, 20, self.texture:getDimensions())
+            }
+        }),
+        ['walking'] = Animation({
+            texture = self.texture,
+            frames = {
+                love.graphics.newQuad(128, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(144, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(160, 0, 16, 20, self.texture:getDimensions()),
+                love.graphics.newQuad(144, 0, 16, 20, self.texture:getDimensions()),
+            },
+            interval = 0.15
+        }),
+        ['jumping'] = Animation({
+            texture = self.texture,
+            frames = {
+                love.graphics.newQuad(32, 0, 16, 20, self.texture:getDimensions())
+            }
+        })
+    }
+
+    -- initialize animation and current frame we should render
+    self.animation = self.animations['idle']
+    self.currentFrame = self.animation:getCurrentFrame()
+
+    -- behavior map we can call based on player state
+    self.behaviors = {
+        ['idle'] = function(dt)
+            
+            -- add spacebar functionality to trigger jump state
+            if love.keyboard.wasPressed('space') then
+                self.dy = -JUMP_VELOCITY
+                self.state = 'jumping'
+                self.animation = self.animations['jumping']
+                self.sounds['jump']:play()
+            elseif love.keyboard.isDown('left') then
+                self.direction = 'left'
+                self.dx = -WALKING_SPEED
+                self.state = 'walking'
+                self.animations['walking']:restart()
+                self.animation = self.animations['walking']
+            elseif love.keyboard.isDown('right') then
+                self.direction = 'right'
+                self.dx = WALKING_SPEED
+                self.state = 'walking'
+                self.animations['walking']:restart()
+                self.animation = self.animations['walking']
+            else
+                self.dx = 0
+            end
+        end,
+        ['walking'] = function(dt)
+            
+            -- keep track of input to switch movement while walking, or reset
+            -- to idle if we're not moving
+            if love.keyboard.wasPressed('space') then
+                self.dy = -JUMP_VELOCITY
+                self.state = 'jumping'
+                self.animation = self.animations['jumping']
+                self.sounds['jump']:play()
+            elseif love.keyboard.isDown('left') then
+                self.direction = 'left'
+                self.dx = -WALKING_SPEED
+            elseif love.keyboard.isDown('right') then
+                self.direction = 'right'
+                self.dx = WALKING_SPEED
+            else
+                self.dx = 0
+                self.state = 'idle'
+                self.animation = self.animations['idle']
+            end
+
+            -- check for collisions moving left and right
+            self:checkRightCollision()
+            self:checkLeftCollision()
+
+            -- check if there's a tile directly beneath us
+            if not self.map:collides(self.map:tileAt(self.x, self.y + self.height)) and
+                not self.map:collides(self.map:tileAt(self.x + self.width - 1, self.y + self.height)) then
+                
+                -- if so, reset velocity and position and change state
+                self.state = 'jumping'
+                self.animation = self.animations['jumping']
+            end
+        end,
+        ['jumping'] = function(dt)
+            -- break if we go below the surface
+            if self.y > 300 then
+                return
+            end
+
+            if love.keyboard.isDown('left') then
+                self.direction = 'left'
+                self.dx = -WALKING_SPEED
+            elseif love.keyboard.isDown('right') then
+                self.direction = 'right'
+                self.dx = WALKING_SPEED
+            end
+
+            -- apply map's gravity before y velocity
+            self.dy = self.dy + self.map.gravity
+
+            -- check if there's a tile directly beneath us
+            if self.map:collides(self.map:tileAt(self.x, self.y + self.height)) or
+                self.map:collides(self.map:tileAt(self.x + self.width - 1, self.y + self.height)) then
+                
+                -- if so, reset velocity and position and change state
+                self.dy = 0
+                self.state = 'idle'
+                self.animation = self.animations['idle']
+                self.y = (self.map:tileAt(self.x, self.y + self.height).y - 1) * self.map.tileHeight - self.height
+            end
+
+            -- check for collisions moving left and right
+            self:checkRightCollision()
+            self:checkLeftCollision()
+        end
+    }
+end
+
